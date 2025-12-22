269f9f9cbfe988ccc17b1f613441c4a26866f8d5
eve12346 <kms38400097@gmail.com>
2025-12-22 17:45:16 +0900

    feat: 20251222 Java Lesson 시작

--- src/main/java/ch00_starting/main.java
+++ src/main/java/ch00_starting/main.java
@@ -3,5 +3,12 @@ package ch00_starting;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("안녕하세요");
+        System.out.println("1234");
+        System.out.println('1');
+        System.out.println("12");
+//        System.out.println(123456);
+//        System.out.println(hello);
+        // ch01_variable 패키지를 만들고 Main 클래스를 생성하시오.
     }
 }
